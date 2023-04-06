9d74813e7d44187fe9d5e1611138220f78dfcff3
bolg55 <bolger.kellen@gmail.com>
2023-04-06 00:56:18 -0400

    config next/font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-montserrat)', 'sans-serif'],
+      },
+      colors: {
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        primary: '#B63E96', // 240,86,199
+        primaryDark: '#58E6D9', // 80,230,217
+      },
+    },
   },
   plugins: [],
 };
