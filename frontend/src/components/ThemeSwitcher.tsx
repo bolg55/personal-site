ca7e23b10f97e398032526975bd5033e05ac09d1
bolg55 <bolger.kellen@gmail.com>
2023-04-07 16:22:53 -0400

    change icon style

--- frontend/src/components/ThemeSwitcher.tsx
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -26,15 +26,15 @@ const ThemeSwitcher: React.FC = () => {
     <button
       aria-label='Toggle Dark Mode'
       type='button'
-      className='shadow p-1 dark:bg-dark bg-light rounded-md shadow-primary dark:shadow-primaryDark hover:shadow-primaryDark dark:hover:shadow-primary transition-all duration-150 ease-in-out'
+      className='shadow p-1 dark:bg-dark bg-light dark:hover:bg-primary/20 hover:bg-primaryDark/10 rounded-md shadow-primary dark:shadow-primaryDark hover:shadow-primaryDark dark:hover:shadow-primary transition-all duration-150 ease-in-out'
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       {!theme ? (
         <HiOutlineSparkles className='h-6 w-6 text-dark dark:text-light' />
       ) : theme === 'dark' ? (
-        <HiOutlineSun className='h-6 w-6 text-dark dark:text-light dark:hover:text-light/80' />
+        <HiOutlineSun className='h-6 w-6 text-dark dark:text-light' />
       ) : (
-        <HiOutlineMoon className='h-6 w-6 text-dark dark:text-light hover:text-dark/40' />
+        <HiOutlineMoon className='h-6 w-6 text-dark dark:text-light' />
       )}
     </button>
   );
