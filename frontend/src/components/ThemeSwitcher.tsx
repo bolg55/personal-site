7394473b2ad80cd4f4323ac68e6b8253d7343d92
bolg55 <bolger.kellen@gmail.com>
2023-04-17 00:24:53 -0400

    fix theme switcher

--- frontend/src/components/ThemeSwitcher.tsx
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -1,40 +1,24 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
-import {
-  HiOutlineSun,
-  HiOutlineMoon,
-  HiOutlineSparkles,
-} from 'react-icons/hi2';
+import { HiOutlineSun, HiOutlineMoon } from 'react-icons/hi2';
 
-const useThemeSwitcher = (): [string, (theme: string) => void] => {
-  const [mode, setMode] = useState('');
+const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => {
-    setMode(theme!);
-  }, [theme]);
-
-  return [mode, setTheme];
-};
-
-const ThemeSwitcher: React.FC = () => {
-  const [theme, setTheme] = useThemeSwitcher();
+  const changeTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
 
   return (
     <button
       aria-label='Toggle Dark Mode'
       type='button'
-      className='shadow p-1 dark:bg-dark bg-light dark:hover:bg-primary/20 hover:bg-primaryDark/10 rounded-md shadow-primary dark:shadow-primaryDark hover:shadow-primaryDark dark:hover:shadow-primary transition-all duration-150 ease-in-out'
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      className='p-1 transition-all duration-150 ease-in-out rounded-md shadow dark:bg-dark bg-light dark:hover:bg-primary/20 hover:bg-primaryDark/10 shadow-primary dark:shadow-primaryDark hover:shadow-primaryDark dark:hover:shadow-primary'
+      onClick={changeTheme}
     >
-      {!theme ? (
-        <HiOutlineSparkles className='h-6 w-6 text-dark dark:text-light' />
-      ) : theme === 'dark' ? (
-        <HiOutlineSun className='h-6 w-6 text-dark dark:text-light' />
+      {theme === 'dark' ? (
+        <HiOutlineSun className='w-6 h-6 text-dark dark:text-light' />
       ) : (
-        <HiOutlineMoon className='h-6 w-6 text-dark dark:text-light' />
+        <HiOutlineMoon className='w-6 h-6 text-dark dark:text-light' />
       )}
     </button>
   );
