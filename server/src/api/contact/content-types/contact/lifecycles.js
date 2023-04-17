05c6f08c9e8d867be2f35aeef2e94376e5e94d14
bolg55 <bolger.kellen@gmail.com>
2023-04-17 19:57:38 -0400

    add contact list

---
+++ server/src/api/contact/content-types/contact/lifecycles.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async afterCreate(event) {
+    const { result } = event;
+
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "kellen@kellenbolger.ca",
+        from: result.email,
+        subject: `[CONTACT FORM] ${result.name}`,
+        replyTo: result.email,
+        text: `New message from ${result.name} (${result.email}):\n\n
+        ${result.message}
+        `,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
