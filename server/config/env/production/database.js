344206e6591b911be8a7e111b6287f6861a73d97
bolg55 <bolger.kellen@gmail.com>
2023-04-16 23:21:30 -0400

    revert config

--- server/config/env/production/database.js
+++ server/config/env/production/database.js
@@ -1,18 +1,13 @@
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      connectionString: env("DATABASE_URL"),
+      host: env("PGHOST", "localhost"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgress"),
+      password: env("PGPASSWORD", "password"),
     },
   },
 });
