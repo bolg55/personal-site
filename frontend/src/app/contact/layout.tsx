3ffa3e3ccdc72dfbc238294026ddf03324f9d684
bolg55 <bolger.kellen@gmail.com>
2023-04-17 23:55:26 -0400

    fix contact meta

---
+++ frontend/src/app/contact/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'Contact Me',
+  description:
+    "Get in touch for your web development needs. Specializing in JavaScript, TypeScript, Node, Next.js, React, and Solid, I'm ready to bring your project to life.",
+};
+
+const ContactLayout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default ContactLayout;
